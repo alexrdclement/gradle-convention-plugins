2711baf550a2c557fcf5f9a986b70d18a54a0497
Alex Clement <alexrdclement@gmail.com>
2025-11-12 09:30:44 -0500

    Add version catalogs fallback

--- plugins/src/main/kotlin/com/alexrdclement/gradle/plugin/ProjectExtensions.kt
+++ plugins/src/main/kotlin/com/alexrdclement/gradle/plugin/ProjectExtensions.kt
@@ -5,5 +5,8 @@ import org.gradle.api.artifacts.VersionCatalog
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.getByType
 
-val Project.libs
-    get(): VersionCatalog = extensions.getByType<VersionCatalogsExtension>().named("alexrdclementPluginLibs")
+val Project.libs: VersionCatalog
+    get() {
+        val catalogs = extensions.getByType<VersionCatalogsExtension>()
+        return catalogs.find("alexrdclementPluginLibs").orElse(catalogs.named("libs"))
+    }
